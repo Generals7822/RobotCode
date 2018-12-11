9f73b51b027057c330b81a3a1a5c5fa4749a975b
erica_luo <erica_luo@1322-HW.hwschools.lan>
2018-12-11 14:53:12 -0500

    el gordo

--- RobotCode/src/Sophia.java
+++ RobotCode/src/Sophia.java
@@ -3,5 +3,6 @@ public class Sophia {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("I am Sophia");
+		System.out.println("cuatro millones");
 }
 }
