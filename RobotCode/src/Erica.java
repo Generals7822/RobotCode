5baa517f1bfcab281a23b8007adf7bd21b0a8574
sophia_szady <sophia_szady@1324-HW.hwschools.lan>
2018-12-11 14:55:42 -0500

    erica is cool now

--- RobotCode/src/Erica.java
+++ RobotCode/src/Erica.java
@@ -3,7 +3,7 @@ public class Erica {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Erica is a loser, does she even know how to use git hub?");
+		System.out.println("erica is pretty cool, she knows how to use github");
 
 	}
 
