6c1219c28274920f6b60dc6ea201003d900a6b0b
sophia_szady <sophia_szady@1324-HW.hwschools.lan>
2018-12-11 14:53:46 -0500

    erica lol

--- RobotCode/src/Erica.java
+++ RobotCode/src/Erica.java
@@ -3,7 +3,7 @@ public class Erica {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("h:(ds");
+		System.out.println("Erica is a loser, does she even know how to use git hub?");
 
 	}
 
