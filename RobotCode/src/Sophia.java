b10487e29abc9b0cef80790b8e8b75a950b0ed82
erica_luo <erica_luo@1322-HW.hwschools.lan>
2018-12-11 14:57:18 -0500

    -----

--- RobotCode/src/Sophia.java
+++ RobotCode/src/Sophia.java
@@ -4,5 +4,6 @@ public class Sophia {
 		// TODO Auto-generated method stub
 		System.out.println("I am Sophia");
 		System.out.println("cuatro millones");
+		System.out.println("this is like bad twitter messaging");
 }
 }
