a2320f12e7fd3bbbb4ec661b0f6c5280ebc086b4
Richard Paul <rdp8172@gmail.com>
2019-04-15 15:13:05 -0400

    Auto Relative to Pixy

--- src/main/java/frc/robot/commands/AutonomousCode.java
+++ src/main/java/frc/robot/commands/AutonomousCode.java
@@ -17,17 +17,15 @@ import io.github.pseudoresonance.pixy2api.Pixy2CCC.Block;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
- * All ToDo Functions must take in a double and return void, all changes should be
- * made through field variables
- * This is a first attempt at Auto Code, all of this is untested and needs testing
- * /bug fixes in order to work
- * Requires: Gyro, Encoder, Pixy, and Ultrasonic
- * Program computes distance and angle based on a Center of Mass(a point that
- * distance and angle can be computed around)
+ * All ToDo Functions must take in a double and return void, all changes should
+ * be made through field variables This is a first attempt at Auto Code, all of
+ * this is untested and needs testing /bug fixes in order to work Requires:
+ * Gyro, Encoder, Pixy, and Ultrasonic Program computes distance and angle based
+ * on a Center of Mass(a point that distance and angle can be computed around)
  * 
  */
 
-public class AutonomousCode { 
+public class AutonomousCode {
 	public static boolean aPressed = false;
 	public static boolean autoMode = false;
 	public static ADIS16448_IMU imu = new ADIS16448_IMU();
@@ -53,20 +51,29 @@ public class AutonomousCode {
 		if (robotAngle > 180) {
 			robotAngle = robotAngle - 360;
 		}
+		double absoluteHatchAngle;
+		do {
+			absoluteHatchAngle = getHatchAngle();// Comes from PixyCam
+		} while (absoluteHatchAngle == Double.MAX_VALUE);//repeats until we get an actual value for Hatch location
+		//Needs to be rethought, becuase it could loop for longer than periodic, and crash
 
-		double hatchAngle = robotAngle + getHatchAngle();// Comes from PixyCam
+		double hatchAngle = absoluteHatchAngle + robotAngle;
+
+		//The Robot currently calulates Distances and Movements relative to the Pixy2, ideally this should be changed to the hook
+		//One of doing this easily might be two Ultrasonics, or more advanced vision processing
 
 		boolean driveToRight = hatchAngle > 0;// Sees if we are driving to the left or right
 		hatchAngle = Math.abs(hatchAngle);// changes everything to positive
-		double ultDistance = ult.getRangeInches();//The distance from the ultrasonic to the cargo ship
-		double CoMtoUltDistance = 0;//Constant to set: distance between Robot Center of Mass and Ultrasonic
-		double angleFromCoMtoUlt = 0;//Constant to set: angle from Center of Mass to Ultrasonic, between 0 and 90
-		double distanceToCargoForward = Math.sqrt(ultDistance * ultDistance + CoMtoUltDistance * CoMtoUltDistance
-				- 2 * CoMtoUltDistance * ultDistance * Math.cos((angleFromCoMtoUlt + 90) * Math.PI / 180));
-		double shipDistance = distanceToCargoForward * Math.cos(robotAngle * Math.PI / 180); // distance of Robot to Cargo
-																							// Ship
+		double ultDistance = ult.getRangeInches();// The distance from the ultrasonic to the cargo ship
+		double PixytoUltDistance = 0;// Constant to set: distance between Pixy and Ultrasonic
+		double angleFromPixytoUlt = 0;// Constant to set: angle from Pixy to Ultrasonic, between 0 and 90
+		double distanceToCargoFromPixy = Math.sqrt(ultDistance * ultDistance + PixytoUltDistance * PixytoUltDistance
+		- 2 * PixytoUltDistance * ultDistance * Math.cos((angleFromPixytoUlt + 90) * Math.PI / 180));
+		double shipDistance = distanceToCargoFromPixy * Math.cos(robotAngle * Math.PI / 180); 
+		//Distance of Robot to Cargo Ship, from Pixy
 		double hatchAngleRad = Math.PI * hatchAngle / 180;// Convert to Radians
-		double inchesFromDrop = 5; // Constant to Set: how far in front the robot should be of the cargo ship when lining up
+		double inchesFromDrop = 5; // Constant to Set: how far in front the robot should be of the cargo ship when
+									// lining up
 
 		double pathDistance = Math.sqrt(shipDistance * shipDistance * Math.tan(hatchAngleRad) * Math.tan(hatchAngleRad)
 				+ (shipDistance - inchesFromDrop) * (shipDistance - inchesFromDrop)); // Formula for calculating the
@@ -162,16 +169,17 @@ public class AutonomousCode {
 		// Complete w/ PixyCam
 		// Test Pixy Code
 
-		if (!SmartDashboard.getString("DB/String 2", "null").equals("Running")) {
-			SmartDashboard.putString("DB/String 2", "Running");
+		// if (!SmartDashboard.getString("DB/String 2", "null").equals("Running")) {
+		// SmartDashboard.putString("DB/String 2", "Running");
 
-		}
+		// }
 
 		ArrayList<Block> blocks = RobotMap.pixy.getCCC().getBlocks();
-		if (!SmartDashboard.getString("DB/String 1", "null").equals("" + blocks.size())) {
-			SmartDashboard.putString("DB/String 1", "" + blocks.size());
+		// if (!SmartDashboard.getString("DB/String 1", "null").equals("" +
+		// blocks.size())) {
+		// SmartDashboard.putString("DB/String 1", "" + blocks.size());
 
-		}
+		// }
 		if (blocks.size() >= 2) {// Intial version, stands to be improved
 			Block leftBlock;
 			Block rightBlock;
@@ -182,12 +190,10 @@ public class AutonomousCode {
 				leftBlock = blocks.get(1);
 				rightBlock = blocks.get(0);
 			}
-			return (30 / 158 * (leftBlock.getX() + leftBlock.getX()) / 2 - 158);// Uses Pixy2 FOV of 30 deg and
-																				// resolution of 316 to calculate approx
-																				// Angle
-
+			return (30 / 158 * (leftBlock.getX() + leftBlock.getX()) / 2 - 158);
+			// Uses Pixy2 FOV of 30 deg and resolution of 316 to calculate approx Angle
 		}
-		return Double.MAX_VALUE;
+		return Double.MAX_VALUE;// return Max Value if there is not two blocks
 
 	}
 
